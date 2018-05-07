b91397e62eb1bb6001c62ff05a09b2a5db46be04
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-05-07 10:32:12 +0200

    fundamental routes for functionality in porgress

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+// later add mongoose
 
 const packagesRoutes = require('./routes/packagesRoutes');
 
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
-app.get('/', (req,res)=>{
-    res.send('Hello world!');
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Request-With, Content-type, Accept, Authorization");
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        res.status(200).json({});
+    }
+
+    next();
 });
 
-app.use('/packages', packagesRoutes);
+app.get('/', (req,res)=>{
+   res.status(200).json({
+       routes: {
+           '/deliver':'GET, POST',
+           "/public/prices.pdf":"GET"
+       }
+   })
+});
+
+app.use('/deliver', packagesRoutes);
+app.use('/public', express.static('public'));
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+   res.status(error.status || 500);
+   res.json({
+       error: {
+           message: error.message
+       }
+   })
+});
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{
-	console.log(`Server listening on ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
 });
--- routes/packagesRoutes.js
+++ routes/packagesRoutes.js
@@ -2,7 +2,46 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req,res)=>{
-    res.send('Packages input!');
+    res.status(200).json({
+        description: "Packages routes for calculating price and number of trucks",
+        requests: {
+            "Creating Packages Delivery": {
+                type: 'POST',
+                path: '/deliver',
+                exampleBody: [
+                    { "id": "ID-1", "weight": 345 },
+                    { "id": "OTHER-ID-2", "weight": 500 },
+                    { "id": "CLIENT-ID-3", "weight": 300 },
+                ]
+            }
+        }
+    });
 });
+
+router.post('/', async (req,res)=>{
+    let packages = req.body;
+    let totalWeight = 0;
+    let price = 0;
+    await packages.map(package => {
+        totalWeight += package.weight;
+        console.log(package);
+    });
+    price = calculatePrice(totalWeight);
+    //body: array of package objects
+    //response: price and trucks
+    res.status(200).json({
+        "price": price,
+        "weight":totalWeight
+    });
+});
+
+
+const calculatePrice = (weight) => {
+  if(weight > 400){
+      return 2+ (0.005 * weight);
+  }
+  return weight * 0.01;
+};
+
 
 module.exports = router;
